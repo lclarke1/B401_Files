0411fe680b3b371f31d7bd0eedd0cae732401b83
lclarke1 <43259482+lclarke1@users.noreply.github.com>
2021-03-04 15:30:03 -0700

    Update compareTNF.R with comments

--- Biostrings/compareTNF.R
+++ Biostrings/compareTNF.R
@@ -1,9 +1,46 @@
- library(Biostrings)
+#This script will calculate the pearson correlation coefficient for all viral regions
+
+#######################################################################################
+#######################################################################################
+
+### Sections
+
+# 1. File Set up before running script
+# 2. Install &Load required Packages & Import Data
+# 3. Run calculation
+# 4. Export to csv
+#######################################################################################
+#######################################################################################
+
+#Section 1: File Set up before running script
+#Copy viral regions .fna files to a folder
+#Create a single file named X_Eukaryotic_Region.fasta containing a set of large eukaryotic contigs
+#Copy X_Eukaryotic_Region.fasta to the viral regions folder
+
+#######################################################################################
+#######################################################################################
+
+# 2. Install &Load required Packages & Import Data
+#    The TetSocTNF is essentially a matrix containing the TNF for all viral regions
+
+install.packages("biostrings")
+
+library(Biostrings)
  vRegions<-list.files(path="C:\\Users\\USERNAME\\FOLDERNAME", full.names=TRUE)
  files <- open_input_files(vRegions)
  TetSoc<-readDNAStringSet(files)
  TetSocTNF <- oligonucleotideFrequency(TetSoc,4, step=300)
  TetSocTNF[1,]                                                
+
+#######################################################################################
+#######################################################################################
+# 3. Run calculation
+#    Note: You will need to adjust the calculation range.
+#          The upper range of the for loop will be the number of viral regions in the folder
+#          The index here for file 93 is referring to the eukaryotic regions file
+#          Example
+#          If you have 10 viral regions, and the eukarotic region file is the 11th file
+#          The for loop range will be 1:10 and all entries that reference region 93 become 11. 
 
 pearson<-0
 for(i in 1:92){
@@ -20,8 +57,19 @@ for(i in 1:92){
         
 }
 
+#######################################################################################
+#######################################################################################
+# 4. Export sorted results to csv
+
 sPearson <-sort(pearson)
 print(sPearson)
 write.table(sPearson, file = "C:\\Users\\USERNAME\\FOLDERNAME\\Output\\output.csv", col.names=FALSE)
+
+#######################################################################################
+#######################################################################################
+
+
+
+
 
 
